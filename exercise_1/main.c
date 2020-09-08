d1541ec44c3864e63eaee6b6d4495a87996bb7ba
Labplass 24 <24@sanntidslab.ntnu>
2020-09-08 14:48:00 +0200

    custom busy_wait WIP

---
+++ exercise_1/main.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <time.h>
+#include <sys/times.h>
+
+struct timespec timespec_normalized(time_t sec, long nsec){
+	while(nsec >= 1000000000){
+		nsec -= 1000000000;
+		++sec;
+	}
+	while(nsec < 0){
+		nsec += 1000000000;
+		--sec;
+	}
+	return (struct timespec){sec, nsec};
+}
+
+
+struct timespec timespec_sub(struct timespec lhs, struct timespec rhs){
+	return timespec_normalized(lhs.tv_sec - rhs.tv_sec, lhs.tv_nsec - rhs.tv_nsec);
+}
+
+
+struct timespec timespec_add(struct timespec lhs, struct timespec rhs){
+	return timespec_normalized(lhs.tv_sec + rhs.tv_sec, lhs.tv_nsec + rhs.tv_nsec);
+}
+
+
+int timespec_cmp(struct timespec lhs, struct timespec rhs){
+	if (lhs.tv_sec < rhs.tv_sec)
+		return -1;
+	if (lhs.tv_sec > rhs.tv_sec)
+		return 1;
+	return lhs.tv_nsec - rhs.tv_nsec;
+}
+
+
+void busy_wait(struct timespec t){
+	struct timespec now;
+	clock_gettime(CLOCK_MONOTONIC, &now);
+	struct timespec then = timespec_add(now, t);
+	while(timespec_cmp(now, then) < 0){
+		for(int i = 0; i < 10000; i++){}
+		clock_gettime(CLOCK_MONOTONIC, &now);
+	}
+}
+
+void busy_wait_times(int sec){
+	struct tms tmp = {0};
+
+	unsigned long long start_time = times(&tmp);
+	unsigned long long future = 0;
+
+	printf("now: utime=%d, stime=%d, return=%d\n", tmp.tms_utime, tmp.tms_stime, start_time);
+	//struct timespec then = timespec_add(now, t);
+	while(future - start_time > sec*100){
+		for(int i = 0; i < 10000; i++){}
+		future = times(&tmp);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	struct timespec t1, t2;
+	struct tms times_s = {0};
+
+	t1.tv_sec = 1;
+	t1.tv_nsec = 0;
+
+	t2.tv_nsec = 0;
+
+	//sleep(1);
+	//usleep(1000000);
+	//nanosleep(&t1, &t2);
+	
+	//busy_wait(t1);
+	busy_wait_times(2);
+
+
+	return 0;
+}
